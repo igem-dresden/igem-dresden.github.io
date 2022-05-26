a393e97c6d9b00fe211d4d81d3643a52a69e3692
Eric Johne <ericjohne2001@gmail.com>
2022-05-27 00:46:38 +0200

    new image added; iGEM logo added, Scroll logic expanded, slides designed, at least some elements

--- scripts/scroll_Logic.js
+++ scripts/scroll_Logic.js
@@ -1,5 +1,5 @@
 Element.prototype.scrollIntoCenter = function() {
-    let options = {"top": this.getCoordinates().y * 0.5, behavior: "smooth"}
+    let options = {"top": this.getCoordinates().y - window.innerHeight * 0.5, behavior: "smooth"}
     window.scroll(options);
 }
 
@@ -9,3 +9,38 @@ Element.prototype.getCoordinates = function() {
     return {x : box.left + scrollX,
             y : box.top + scrollY}
 }
+
+function appearWhenInView () {
+    var appearElements = document.querySelectorAll(".appear-left, .appear-right");
+    for(i = 0; i < appearElements.length; i++) {
+        let position = appearElements[i].getBoundingClientRect().top;
+        if(position < window.innerHeight - 150) {
+            appearElements[i].classList.add("active");
+        } 
+        else {
+            appearElements[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", appearWhenInView);
+window.addEventListener("load", appearWhenInView);
+
+window.onscroll = () => {
+    let headlines = document.querySelectorAll('.headline');
+    let menu = document.querySelectorAll('.menu_bar li');
+    headlines.forEach(i => {
+        let top = window.scrollY;
+        let offset = i.offsetTop - window.innerHeight * 0.15;
+        let height = i.offsetHeight;
+        let id = i.getAttribute('id');
+        console.log("Hey")
+        if (top >= offset && top < offset + height) {
+            menu.forEach(link => {
+            link.classList.remove('active');
+            document.getElementById(id + "-m").classList.add('active');
+            console.log(id + "-m");
+            });
+        }
+    });
+};
