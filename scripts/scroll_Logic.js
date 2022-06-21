a003b9559bb54cc1ed0dfae58e5674c9d8432819
Eric Johne <ericjohne2001@gmail.com>
2022-06-21 13:01:39 +0200

    Animation Basic for Solution Slider added; Additionally solved display bug in Edge and Chrome by removing display: none from main svg at the end of the file

--- scripts/scroll_Logic.js
+++ scripts/scroll_Logic.js
@@ -1,3 +1,12 @@
+window.addEventListener("scroll", appearWhenInView);
+window.addEventListener("load", appearWhenInView);
+window.addEventListener('scroll', parallaxScroll);
+window.addEventListener('scroll', function(e)  {
+    linkedScrollBehaviour("#detail-anchor", ".bacteriophage, .growth_factor", "active", "remove");
+    linkedScrollBehaviour(".wound", ".bacteriophage, .growth_factor, .wound", "active2", "add");
+});
+
+
 Element.prototype.scrollIntoCenter = function() {
     let options = {"top": this.getCoordinates().y - window.innerHeight * 0.5, behavior: "smooth"}
     window.scroll(options);
@@ -8,6 +17,18 @@ Element.prototype.getCoordinates = function() {
 
     return {x : box.left + scrollX,
             y : box.top + scrollY}
+}
+
+function parallaxScroll() {
+        
+    const target = document.querySelectorAll('.scrollItem'); // EVERY ITEM with class "scrollItem" will get parallaxEffect
+
+    for(let i = 0; i < target.length; i++) {
+        var scrollweight = Number(target[i].dataset['scrollweight']); // the attribute data-scrollweight will determine the certain parallax effect speed of the element
+        var scrollRate = `${(window.pageYOffset / window.innerHeight) * 100 * scrollweight}%`;
+
+        target[i].style.transform = 'translate3d(0px, ' + scrollRate + ', 0px)';
+    }
 }
 
 function appearWhenInView () {
@@ -23,50 +44,20 @@ function appearWhenInView () {
     }
 }
 
-window.addEventListener("scroll", appearWhenInView);
-window.addEventListener("load", appearWhenInView);
-
-/* 
- * ==============================================================================
- * Parallax Scroll
- * ==============================================================================
- * 
- * This little script will provide the functionality to create a 
- * parallax effect for html elements.
- * To create such effect, elements must have the class scrollItem 
- * and the custom attribute data-scrollWeight
- * to determine their parallax speed. 
- * This attribute must be a number to have an effect on the element's movement.
-*/
-
-window.addEventListener('scroll', function(e) {
-        
-    const target = document.querySelectorAll('.scrollItem');
-
-    for(let i = 0; i < target.length; i++) {
-        var scrollweight = Number(target[i].dataset['scrollweight']);
-        var scrollRate = `${(window.pageYOffset / window.innerHeight) * 100 * scrollweight}%`;
-
-        target[i].style.transform = 'translate3d(0px, ' + scrollRate + ', 0px)';
-    }
-});
-
-/*
-window.addEventListener('scroll', () => {
-    let headlines = document.querySelectorAll('.headline');
-    let menu = document.querySelectorAll('.menu_bar li');
-    headlines.forEach(i => {
-        let top = window.scrollY;
-        let offset = i.offsetTop - window.innerHeight * 0.15;
-        let height = i.offsetHeight;
-        let id = i.getAttribute('id');
-        if (top >= offset && top < offset + height) {
-            menu.forEach(link => {
-            link.classList.remove('active');
-            document.getElementById(id + "-m").classList.add('active');
-            console.log(id + "-m");
-            });
+function linkedScrollBehaviour(anchorQuery, linkQuery, behaviourClass, opererator="add") {
+    const anchorElements = document.querySelectorAll(anchorQuery)
+    const linkElements = document.querySelectorAll(linkQuery)
+    for(i = 0; i < anchorElements.length; i++) {
+        let position = anchorElements[i].getBoundingClientRect().top;
+        if(position < window.innerHeight - 300) {
+            for(j = 0; j < linkElements.length; j++) {
+                (opererator === "add") ? linkElements[j].classList.add(behaviourClass) : linkElements[j].classList.remove(behaviourClass);
+            }
         }
-    });
-});
-*/
+        else {
+            for(j = 0; j < linkElements.length; j++) {
+                (opererator === "add") ? linkElements[j].classList.remove(behaviourClass) : linkElements[j].classList.add(behaviourClass);
+            }
+        }
+    }
+}
